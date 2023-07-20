2c46672ea466857841ed2998136f21fef14dc6f6
Progress with the component <deMiguel@newsaktuell.de>
2023-07-20 16:47:06 +0200

    init commit

---
+++ app/anmeldung/page.tsx
@@ -0,0 +1,265 @@
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Anmeldung() {
+  return (
+    <div className={styles.contentContainer}>
+      <Link href="/" className={styles.goHomeButton}>
+        Inicio
+      </Link>
+      <h1>An, Um y Abmeldung en Alemania</h1>
+      <p>
+        <strong>Anmeldung</strong>
+      </p>
+      <p>
+        Cuando uno llega nuevo nuevito a Alemania (excepto turistas) lo primero
+        que hay que hacer es registrar el domicilio. El registro o en alemán
+        “Anmeldung”* es un trámite que se hace en las oficinas de la
+        administración pública del municipio en el que uno va a vivir
+        (Einwohnerangelegenheiten).
+      </p>
+      <p>
+        “Anmeldung” en alemán significa (entre otras cosas) registro,
+        inscripción, y es una palabra que puede aplicar a muuuuchísimos
+        contextos (ej: registro/inscripción a un curso, a la facu, a un evento,
+        mil cosas). En este caso me voy a referir al registro del domicilio /
+        lugar de residencia.
+      </p>
+      <p>
+        Este trámite es muy importante es porque es el trámite que activa una
+        bola de nieve de procesos administrativos como:
+      </p>
+      <ul>
+        <li>Tu número de identificación fiscal (Steuer-ID)</li>
+        <li>Tu número de seguridad social (Sozialversicherungs­ausweis)</li>
+      </ul>
+      <p>
+        Estas identificaciones son necesarias para otras cosas como por ejemplo
+        trabajar.
+      </p>
+      <p>
+        Aunque no es 100% un requisito, la verdad es que hay muchísimos
+        empleadores que no te van a tomar hasta que tengas estas cosas. Pero en
+        mi experiencia y la de algunos otros, existen también algunos que te
+        dejan empezar mientras arreglas esos temas de papeles, pero para lo que
+        seguro lo vas a necesitar es para que te puedan pagar el sueldo.
+      </p>
+      <p>Ahora, yendo a lo concreto. ¿Cómo se hace el Anmeldung?</p>
+      <p>En Hamburgo para poder registrarte vas a necesitar:</p>
+      <ul>
+        <li>
+          Documento de identificación personal: Pasaporte si sos extranjero
+        </li>
+        <li>
+          <a href="https://welcome.hamburg.de/contentblob/2217296/42d1e8e18938d0d8b85e1d0c600755e2/data/anmeldung-des-wohnsitzes.pdf">
+            Formulario de registración “Anmeldevordruck”
+          </a>
+        </li>
+        <li>
+          Un formulario firmado por quien te alquila tu departamento /
+          habitación / donde sea que vayas a vivir: el
+          "Wohnungsgeberbestätigung"{" "}
+          <a href="https://www.hamburg.de/Dibis/vordr/6700-25-barrierefrei.pdf">
+            (descargar formulario)
+          </a>
+        </li>
+      </ul>
+      <p>
+        Si hay niños en la flia la web dice “En el caso de los niños, debe
+        presentarse el certificado de nacimiento en la forma adecuada
+        (certificado internacional o apostilla/legalización realizada por un
+        traductor jurado en Alemania) y todas las personas sujetas a registro
+        deben comparecer personalmente.”
+      </p>
+      <p>En los siguientes casos también hay que presentar lo siguiente:</p>
+      <ul>
+        <li>Personas casadas/parejas: certificado de matrimonio (original)</li>
+        <li>
+          Personas divorciadas: sentencia de divorcio jurídicamente vinculante
+          (original)
+        </li>
+        <li>Personas viudas: Certificado de defunción (original)</li>
+        <li>
+          Titulares de un permiso de residencia electrónico (eAT): el permiso de
+          residencia
+        </li>
+      </ul>
+      <p>El trámite al día de hoy sale 12 euros.</p>
+      <p>
+        Hoy en día por suerte en Hamburgo hay una plataforma online que te
+        permite sacar turno para ciertos trámites y el Anmeldung es uno de
+        ellos.
+      </p>
+      <p>
+        Para sacar turno tienen que ir acá:{" "}
+        <a href="https://serviceportal.hamburg.de/HamburgGateway/FVP/FV/Bezirke/DigiTermin/Dsgvo">
+          Plataforma de turnos
+        </a>
+      </p>
+      <p>
+        <strong>Ummeldung</strong>
+      </p>
+      <p>
+        El Ummeldung es el cambio de domicilio dentro de la ciudad. Osea si te
+        mudas dentro de Hamburgo tener que elegir en el menu de los turnos
+        “Wohnsitz Ummeldung innerhalb Hamburgs”. Y todo el resto es igual.
+      </p>
+      <p>
+        <strong>Abmeldung</strong>
+      </p>
+      <p>
+        Cuando te vas de Hamburgo, si no te mudas y te vas a registrar en otro
+        lugar de Alemania tenes que hacer el Abmeldung, para eso, en el menu de
+        turnos elegis “Wohnsitz Abmeldung des Nebenwohnsitzes”.
+      </p>
+      <p>
+        <strong>
+          Temas: Registro de domicilio (Empadronamiento) Cambio de domicilio
+          Cancelación de registro
+        </strong>
+      </p>
+      <h2>Registro de domicilio</h2>
+      <p>
+        Cuando uno llega nuevo a Alemania (excepto turistas), lo primero que hay
+        que hacer es empadronarse. El empadronamiento, también conocido como
+        "Anmeldung" en alemán, es un trámite que se realiza en las oficinas de
+        la administración pública del municipio donde uno va a vivir
+        (Einwohnerangelegenheiten). La palabra "Anmeldung" se refiere al
+        registro o inscripción en diferentes contextos (por ejemplo, inscripción
+        en un curso, en la universidad, en un evento, entre otros). En este
+        caso, nos referimos al registro del domicilio o lugar de residencia.
+        Este trámite es muy importante, ya que activa una serie de procesos
+        administrativos, como la obtención del número de identificación fiscal
+        (Steuer-ID) y del número de seguridad social
+        (Sozialversicherungs­ausweis). Estas identificaciones son necesarias
+        para realizar diversas actividades, como trabajar. Aunque no es
+        estrictamente obligatorio, la realidad es que muchos empleadores no te
+        contratarán hasta que tengas estos documentos. Sin embargo, en mi
+        experiencia y en la de otros, también existen empleadores que te
+        permiten comenzar a trabajar mientras arreglas estos trámites
+        burocráticos, pero sí necesitarás tenerlos para poder recibir tu sueldo.
+      </p>
+      <p>
+        Ahora, yendo a lo concreto, ¿cómo se realiza el empadronamiento en
+        Hamburgo? Para poder registrarte en Hamburgo, necesitarás:
+      </p>
+      <ul>
+        <li>Documento de identidad personal: pasaporte si eres extranjero.</li>
+        <li>
+          <a href="https://welcome.hamburg.de/contentblob/2217296/42d1e8e18938d0d8b85e1d0c600755e2/data/anmeldung-des-wohnsitzes.pdf">
+            Formulario de empadronamiento "Anmeldevordruck"
+          </a>
+          .
+        </li>
+        <li>
+          Un formulario firmado por la persona que te alquila el departamento,
+          habitación o lugar donde vivirás, llamado "Wohnungsgeberbestätigung"{" "}
+          <a href="https://www.hamburg.de/Dibis/vordr/6700-25-barrierefrei.pdf">
+            (descargar formulario)
+          </a>
+          . Este formulario incluye los datos del lugar de residencia, de las
+          personas que se registrarán y de la persona que te alquila o aloja.
+        </li>
+      </ul>
+      <p>
+        Si hay niños en la familia, según la página web, se debe presentar el
+        certificado de nacimiento en la forma adecuada (certificado
+        internacional o apostilla/legalización realizada por un traductor jurado
+        en Alemania) y todas las personas sujetas a registro deben comparecer
+        personalmente.
+      </p>
+      <p>
+        En los siguientes casos, también se debe presentar la documentación
+        correspondiente:
+      </p>
+      <ul>
+        <li>Personas casadas/parejas: certificado de matrimonio (original).</li>
+        <li>
+          Personas divorciadas: sentencia de divorcio legalmente vinculante
+          (original).
+        </li>
+        <li>Personas viudas: certificado de defunción (original).</li>
+        <li>
+          Titulares de una tarjeta de residencia electrónica (eAT): la tarjeta
+          de residencia.
+        </li>
+      </ul>
+      <p>
+        El costo actual del trámite es de 12 euros. Afortunadamente, en
+        Hamburgo, existe una plataforma en línea que permite sacar turno para
+        ciertos trámites, incluyendo el empadronamiento. Para solicitar turno,
+        debes ingresar aquí:{" "}
+        <a href="https://serviceportal.hamburg.de/HamburgGateway/FVP/FV/Bezirke/DigiTermin/Dsgvo">
+          Plataforma de turnos
+        </a>
+      </p>
+      <p>Pasos a seguir:</p>
+      <ol>
+        <li>Accede al enlace que se menciona arriba.</li>
+        <li>
+          Acepta las condiciones de protección de datos y haz clic en "Weiter"
+          (Siguiente).
+        </li>
+        <li>
+          Completa los campos de nombre, apellido, correo electrónico y,
+          opcionalmente, teléfono.
+        </li>
+        <li>En el próximo menú, busca la opción "Meldewesen".</li>
+        <li>
+          Donde aparece el 0, elige la cantidad correspondiente de personas (1
+          para una persona, 2 para dos personas, etc.). Si es tu primera vez
+          registrándote en Alemania o en Hamburgo, selecciona la primera opción.
+          Si te estás mudando dentro de la ciudad de Hamburgo, elige la segunda
+          opción.
+        </li>
+        <li>Desplázate hacia abajo hasta que puedas hacer clic en "Weiter".</li>
+        <li>Verifica la disponibilidad de turnos en el mapa y elige uno.</li>
+        <li>
+          Al finalizar, se mostrará un resumen de tu turno. Haz clic en "Termin
+          verbindlich vereinbaren" (Confirmar cita). También recibirás los
+          detalles de tu turno por correo electrónico.
+        </li>
+      </ol>
+      <p>
+        Puedes utilizar la función de traducción de Google Chrome para traducir
+        la página al español. Esto puede ser útil en caso de que encuentres
+        alguna dificultad con la traducción.
+      </p>
+      <p>
+        Ten en cuenta que el sistema de turnos puede ser aleatorio, por lo que
+        puede ser necesario actualizar la página varias veces para encontrar una
+        opción más próxima en el tiempo.
+      </p>
+      <p>Enlaces útiles:</p>
+      <ul>
+        <li>
+          Versión en inglés de las instrucciones en el sitio web del Hamburg
+          Welcome Center:{" "}
+          <a href="https://www.hamburg.com/welcome/entry-residence/general-regulations/11725242/registration/">
+            Instrucciones en inglés
+          </a>
+        </li>
+        <li>
+          Guía paso a paso en inglés:{" "}
+          <a href="https://www.hamburg.com/contentblob/11701420/7306804dcaa6d6881635172d1ad5e7fe/data/km1-appointment-kuz-%E2%80%93-guide-in-english-language.pdf">
+            Guía paso a paso en inglés
+          </a>
+        </li>
+      </ul>
+      <h2>Cambio de domicilio</h2>
+      <p>
+        El cambio de domicilio dentro de la misma ciudad se conoce como
+        Ummeldung. Si te mudas dentro de Hamburgo, debes seleccionar la opción
+        "Wohnsitz Ummeldung innerhalb Hamburgs" en el menú de turnos. El resto
+        del procedimiento es el mismo.
+      </p>
+      <h2>Cancelación de registro</h2>
+      <p>
+        Cuando te vas de Hamburgo y te registras en otro lugar de Alemania,
+        debes realizar la Abmeldung. Para ello, en el menú de turnos, elige
+        "Wohnsitz Abmeldung des Nebenwohnsitzes".
+      </p>
+    </div>
+  );
+}
