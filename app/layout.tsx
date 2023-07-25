4e0645e9d08e7156745504895871569866d0e63f
Joo Han Kim <joo-han.kim@hermesworld.com>
2023-07-25 19:32:49 +0200

    Added tailwind and radix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import { climateCrisis, playfair } from "./fonts";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html
+      lang="en"
+      className={`${playfair.variable} ${climateCrisis.variable}`}
+    >
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,52 @@
-import { Inter } from "@next/font/google";
+// import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <div className="team">by M&A&apos;s Team</div>
-      </div>
-      <form></form>
-      <div className={styles.center}>
-        <h2>Mates & Asado Hamburg 🧉</h2>
-      </div>
+    <>
+      <header className="text-center flex justify-center items-center p-10">
+        <div>
+          <p className="text-center">July 20.0.2023</p>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <div className="team">by M&A&apos;s Team</div>
+        </div>
+        <div className="py-4">
+          <h1 className="text-9xl">Mates & Asado Hamburg 🧉</h1>
+        </div>
 
-      <div className={styles.grid}>
-        <a href="/anmeldung" className={styles.card} rel="noopener noreferrer">
-          <h2 className={inter.className}>
-            An-, Um- y Abmeldung <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Informacion sobre el registro de domicilio en Alemania.
-          </p>
-        </a>
+        <div className={styles.grid}>
+          <a
+            href="/anmeldung"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2 className="font-serif">
+              An-, Um- y Abmeldung <span>-&gt;</span>
+            </h2>
+            <p className="font-serif">
+              Informacion sobre el registro de domicilio en Alemania.
+            </p>
+          </a>
 
-        <a
-          href="/fuehrerschein"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Führerschein <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Como conseguir la licencia en Alemania
-          </p>
-        </a>
-      </div>
-    </main>
+          <a
+            href="/fuehrerschein"
+            className={styles.card}
+            rel="noopener noreferrer"
+          >
+            <h2 className="font-heading">
+              Führerschein <span>-&gt;</span>
+            </h2>
+            <p className="font-playfair">
+              Como conseguir la licencia en Alemania
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
   );
 }
