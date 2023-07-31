42d7774fccea46f03490b45a05e56422bad7a1fe
Progress with the component <deMiguel@newsaktuell.de>
2023-07-31 12:08:55 +0200

    made home responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,31 +3,36 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <header className="text-center flex justify-center items-center p-10">
+      <header className="text-center flex justify-center items-center p-4 md:p-10">
         <div>
-          <p className="text-center text-lg tracking-[5rem] uppercase">
+          <p className="text-center text-lg sm:text-xl lg:text-2xl tracking-normal sm:tracking-wide lg:tracking-wider uppercase">
             by M&A&apos;s Team
           </p>
         </div>
       </header>
-      <main className="text-center px-12">
+
+      <main className="text-center px-4 md:px-12">
         <div className="pt-12">
-          <h1 className="text-9xl font-heading uppercase">Mates & Asado</h1>
+          <h1 className="text-4xl md:text-9xl font-heading uppercase">
+            Mates & Asado
+          </h1>
         </div>
         <div className="py-6">
-          <h3 className="text-2l font-heading uppercase">Comunidad de ayuda en español para emigrados en Hamburgo</h3>
+          <h3 className="text-lg md:text-2xl font-heading uppercase">
+            Comunidad de ayuda en español para emigrados en Hamburgo
+          </h3>
         </div>
 
-        <div className=" py-24 grid gap-40 grid-cols-3 grid-rows-1 px-12">
+        <div className="py-24 grid gap-8 md:gap-40 grid-cols-1 md:grid-cols-3 grid-rows-3 px-4 md:px-12">
           <a
             href="/anmeldung"
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              An-, Um- y Abmeldung <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              An-, Um- y Abmeldung
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Informacion sobre el registro de domicilio en Alemania.
             </p>
           </a>
@@ -36,10 +41,10 @@ export default function Home() {
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              Führerschein <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              Führerschein
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Como tramitar la licencia en Alemania
             </p>
           </a>
@@ -48,10 +53,10 @@ export default function Home() {
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              Wohnungssuche <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              Wohnungssuche
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Busqueda de departamento/WG + como prevenir estafas
             </p>
           </a>
@@ -60,10 +65,10 @@ export default function Home() {
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              Productoslatinos <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              Productoslatinos
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Donde comprar productos latinos en Hamburgo + Online
             </p>
           </a>
@@ -72,10 +77,10 @@ export default function Home() {
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              Teléfonos Útiles <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              Teléfonos Útiles
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Guia de teléfonos útiles para inmigrantes en Hamburgo
             </p>
           </a>
@@ -84,10 +89,10 @@ export default function Home() {
             className="text-center"
             rel="noopener noreferrer"
           >
-            <h2 className="font-serif font-bold">
-              Deutschland Ticket <span>-&gt;</span>
+            <h2 className="font-serif font-bold text-lg md:text-xl">
+              Deutschland Ticket
             </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm">
+            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
               Información sobre el Deutschlandticket
             </p>
           </a>
