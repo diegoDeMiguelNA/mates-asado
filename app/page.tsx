d0e54a86209a01bb99ffd54ef7dd750824f071a1
Progress with the component <deMiguel@newsaktuell.de>
2023-08-13 19:38:47 +0200

    improved icons

--- app/page.tsx
+++ app/page.tsx
@@ -14,90 +14,48 @@ export default function Home() {
             linkTo="/pages/anmeldung"
             contentfulReference="referenceIdForAnmeldungFromContentful"
           />
-          <a
-            href="/pages/anmeldung"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              An-, Um- y Abmeldung
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Informacion sobre el registro de domicilio en Alemania.
-            </p>
-          </a>
-          <a
-            href="/pages/fuehrerschein"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Führerschein
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Como tramitar la licencia de conducir en Alemania
-            </p>
-          </a>
-          <a
-            href="/pages/wohnungssuche"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Wohnungssuche
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Busqueda de departamento/WG + como prevenir estafas
-            </p>
-          </a>
-          <a
-            href="/pages/productos-latinos"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Productoslatinos
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Donde comprar productos latinos en Hamburgo + Online
-            </p>
-          </a>
-          <a
-            href="/pages/telefonos-utiles"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Teléfonos Útiles
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Guia de teléfonos útiles para inmigrantes en Hamburgo
-            </p>
-          </a>
-          <a
-            href="/pages/deutschlandticket"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Deutschland Ticket
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Información sobre el Deutschlandticket
-            </p>
-          </a>
-          <a
-            href="/pages/medicos"
-            className="text-center"
-            rel="noopener noreferrer"
-          >
-            <h2 className="font-serif font-bold text-lg md:text-xl">
-              Médicos en Hamburgo
-            </h2>
-            <p className="text-gray-400 font-thin font-playfair text-sm md:text-base">
-              Info sobre médicos en Hamburgo que hablan español o inglés.
-            </p>
-          </a>
+          <HomeIcon
+            iconSrc="./icons/driving-license.svg"
+            title="Führerschein"
+            description="Como tramitar la licencia de conducir en Alemania"
+            linkTo="/pages/fuehrerschein"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
+          <HomeIcon
+            iconSrc="./icons/house.svg"
+            title="Wohnungssuche"
+            description="Busqueda de departamento/WG + como prevenir estafas"
+            linkTo="/pages/wohnungssuche"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
+          <HomeIcon
+            iconSrc="./icons/mate.svg"
+            title="  Productos Latinos"
+            description="  Donde comprar productos latinos en Hamburgo + Online"
+            linkTo="/pages/productos-latinos"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
+          <HomeIcon
+            iconSrc="./icons/phone.svg"
+            title="Teléfonos Útiles"
+            description="  Guia de teléfonos útiles para inmigrantes en Hamburgo"
+            linkTo="/pages/telefonos-utiles"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
+          <HomeIcon
+            iconSrc="./icons/train.svg"
+            title="Deutschland Ticket"
+            description="Información sobre el Deutschlandticket"
+            linkTo="/pages/deutschlandticket"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
+          <HomeIcon
+            iconSrc="./icons/stethoscope.svg"
+            title="Médicos en Hamburgo"
+              description="Info sobre médicos que hablan español o inglés."
+            linkTo="/pages/medicos"
+            contentfulReference="referenceIdForAnmeldungFromContentful"
+          />
         </div>
       </main>
     </>
