8a4b41f39ab4a31122595e6ebab045494f449ba7
Progress with the component <deMiguel@newsaktuell.de>
2023-08-13 08:13:07 +0200

    refactored code according to joo's idea

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Header from "./header/header";
+import Header from "./components/header/header";
+
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <Header />
         <div className="py-10 md:py-24 grid gap-8 md:gap-40 grid-cols-1 md:grid-cols-3 grid-rows-3 px-4 md:px-12">
           <a
-            href="/anmeldung"
+            href="/pages/anmeldung"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -19,7 +20,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/fuehrerschein"
+            href="/pages/fuehrerschein"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -31,7 +32,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/wohnungssuche"
+            href="/pages/wohnungssuche"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -43,7 +44,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/productos-latinos"
+            href="/pages/productos-latinos"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -55,7 +56,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/telefonos-utiles"
+            href="/pages/telefonos-utiles"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -67,7 +68,7 @@ export default function Home() {
             </p>
           </a>
           <a
-            href="/deutschlandticket"
+            href="/pages/deutschlandticket"
             className="text-center"
             rel="noopener noreferrer"
           >
@@ -78,7 +79,7 @@ export default function Home() {
               Información sobre el Deutschlandticket
             </p>
           </a>
-          <a href="/medicos" className="text-center" rel="noopener noreferrer">
+          <a href="/pages/medicos" className="text-center" rel="noopener noreferrer">
             <h2 className="font-serif font-bold text-lg md:text-xl">
               Médicos en Hamburgo
             </h2>
