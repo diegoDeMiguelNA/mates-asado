75c250aceff1c79dd775d937bf187167c19e571e
Progress with the component <deMiguel@newsaktuell.de>
2023-08-17 15:39:42 +0200

    dekstop view improved

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,16 +1,18 @@
 export default function Header() {
   return (
-    <header className="relative text-white grid grid-rows-header h-[230px]">
-      <div className="relative h-full">
-        <div className="absolute inset-0 bg-cover bg-center opacity-80 h-[210px] bg-[url('/images/port.webp')]" ></div>
-        <div className="flex flex-col justify-center items-center h-full relative z-10">
-          <h1 className="text-3xl md:text-5xl font-heading mb-7.5">Mates & Asado</h1> 
-          <h3 className="order-[-1] text-xs md:text-sm text-center mb-2.5">By M&A&apos;s Team</h3>
-        </div>
+    <header className="relative grid grid-rows-header h-[230px] md:h-[280px] lg:h-[500px]">
+
+      <div className="absolute inset-0 bg-cover bg-center opacity-80 h-[210px] md:h-[260px] lg:h-[480px] bg-[url('/images/port.webp')]" />
+
+      <div className="flex flex-col justify-center items-center h-[190px] md:h-[240px] lg:h-[460px] relative z-10">
+        <h1 className="text-3xl md:text-5xl lg:text-8xl font-heading font-light mb-7.5 order-2 text-white">Mates & Asado</h1> 
+        <h3 className="text-xs md:text-sm text-center mb-2.5 order-1 text-white">By M&A&apos;s Team</h3>
       </div>
-      <div className="bg-customRed w-full flex items-center justify-center h-[20px] md:h-[22px]">
-        <h2 className="text-xxs md:text-sm leading-tight">Comunidad de ayuda para emigrados en Hamburgo</h2>
+
+      <div className="absolute bottom-0 bg-customRed w-full flex items-center justify-center h-[20px] md:h-[22px] lg:h-[38px]">
+        <h2 className="text-xxs md:text-sm lg:text-xl leading-tight text-white">Comunidad de ayuda para emigrados en Hamburgo</h2>
       </div>
+
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,41 @@ export default async function Home() {
     <>
       <main className="text-center">
         <Header />
+
         <HomeProps />
+
         <div className="experiencias-de-vida mb-16">
-          <div className="inset-0 bg-cover bg-center h-[80px] md:h-[140px] bg-[url('/images/rathaus-cut.webp')]" />
-          <div className="h-72 bg-regular-grey flex flex-col px-16 py-10 justify-center items-center">
-            <h3 className="text-s md:text-xl text-regular-red font-bold mb-4">
-              Experiencias de vida
-            </h3>
-            <p className="text-xs md:text-sm md:px-6 font-light mb-4">
-              Conocé relatos inspiradores de compatriotas en Hamburgo: desde
-              valientes que montaron su propio negocio, hasta los que
-              enfrentaron los desafíos del Führerschein y el trámite de
-              Einbürgerung.
-            </p>
-            <Button
-              className="bg-regular-red text-xxs md:text-xs rounded-full mt-4"
-              size="customPill"
-              style={{ color: "white" }}
-            >
-              Coming Soon
-            </Button>
+        
+          <section className="bg-cover bg-center h-[80px] md:h-[140px] lg:hidden bg-[url('/images/rathaus-cut.webp')]" />
+
+          <div className="lg:grid lg:grid-cols-2 h-72 lg:h-[540px] bg-regular-grey px-16 py-10 items-center">
+            
+            <section className="hidden lg:block bg-cover bg-center lg:h-full lg:w-full bg-[url('/images/rathaus-full.webp')]" />
+
+         
+            <div className="flex flex-col items-center lg:items-center justify-center space-y-4">
+              <h3 className="text-s md:text-xl text-regular-red font-bold">
+                Experiencias de vida
+              </h3>
+              <p className="text-xs md:text-sm md:px-6 font-light max-w-[800px]">
+                Conocé relatos inspiradores de compatriotas en Hamburgo: desde
+                valientes que montaron su propio negocio, hasta los que
+                enfrentaron los desafíos del Führerschein y el trámite de
+                Einbürgerung.
+              </p>
+              <Button
+                className="bg-regular-red text-xxs md:text-xs rounded-full"
+                size="customPill"
+                style={{ color: "white" }}
+              >
+                Coming Soon
+              </Button>
+            </div>
           </div>
         </div>
+
         <div className="about-us mb-16 flex flex-col px-16 py-6 justify-center items-center mb-20">
-        <div className="inset-0 bg-cover bg-center h-[120px] md:h-[210px] w-[500px] rounded-2xl bg-[url('/images/rainbow.webp')]" />
+          <section className="inset-0 bg-cover bg-center h-[130px] w-[290px] md:w-[500px] md:h-[210px] rounded-2xl bg-[url('/images/rainbow.webp')]" />
           <h3 className="text-s md:text-xl text-regular-teal font-bold mb-4 mt-12">
             Sobre Nosotros
           </h3>
@@ -44,7 +55,7 @@ export default async function Home() {
             punto de encuentro!
           </p>
           <Button
-             className="bg-regular-teal text-xxs md:text-xs rounded-full mt-4"
+            className="bg-regular-teal text-xxs md:text-xs rounded-full mt-4"
             size="customPill"
             style={{ color: "white" }}
           >
