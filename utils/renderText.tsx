4ee2312b2c5c9e9463c83d3b1c2d0fc3d980e0e1
Progress with the component <deMiguel@newsaktuell.de>
2023-08-22 16:32:51 +0200

    improved paragraph styling

--- utils/renderText.tsx
+++ utils/renderText.tsx
@@ -26,9 +26,11 @@ export const RenderContent = (props: {
 const BlogSubtitleAndParagraph = (props: { fields: any }) => {
   return (
     <div className={clsx(props.fields.subtitle && "mt-10")}>
-      <h2 className="mt-10 mb-8 text-l sm:text-3xl font-heading uppercase sm:mt-16 lg:mt-12">
-        {props.fields.subtitle}
-      </h2>
+      {props.fields.subtitle && (
+        <h2 className="mt-10 mb-16 text-l sm:text-3xl font-heading uppercase sm:mt-16 lg:mt-12">
+          {props.fields.subtitle}
+        </h2>
+      )}
       {props.fields.paragraph.content.map(
         (content: any, index: number) =>
           content.nodeType === "paragraph" &&
@@ -43,44 +45,63 @@ const BlogSubtitleAndParagraph = (props: { fields: any }) => {
 };
 
 const ListWithOrWithoutSubtitle = (props: { fields: any }) => {
-    return (
-      <div className="flex flex-col justify-center px-4 min-h-screen max-w-[550px]">
-        <h2 className="mt-10 sm:text-3xl font-heading uppercase">
+  return (
+    <div className="flex flex-col justify-center px-4 min-h-screen max-w-[550px]">
+      {props.fields.subtitle && (
+        <h2 className="mt-10 sm:text-3xl font-heading uppercase mb-4">
           {props.fields.subtitle}
         </h2>
-        <div className="max-w-xl text-left">
-          {props.fields.list.content.map(
-            (content: any, index: number) =>
-              content.nodeType === "unordered-list" && (
-                <ul
-                  className={clsx(
-                    props.fields.isOrderedList ? "list-decimal pl-0" : "list-disc pl-5",
-                    "py-6 list-inside"
-                  )}
-                  key={index}
-                >
-                  {content.content.map((listItem: any, itemIndex: number) =>
-                    listItem.content.map(
-                      (listContent: any, contentIndex: number) =>
-                        listContent.nodeType === "paragraph" &&
-                        listContent.content.map(
-                          (text: any, textIndex: number) => (
-                            <li className={clsx(props.fields.isOrderedList ? "flex my-2" : "", "relative")} key={textIndex}>
-                              {props.fields.isOrderedList && <span className="mr-2">{itemIndex + 1}.</span>}
-                              <span>{text.value}</span>
-                            </li>
-                          )
+      )}
+      <div className="max-w-xl text-left">
+        {props.fields.list.content.map(
+          (content: any, index: number) =>
+            content.nodeType === "unordered-list" && (
+              <ul
+                className={clsx(
+                  props.fields.isOrderedList
+                    ? "list-decimal list-inside pl-0"
+                    : "list-disc list-outside",
+                  "py-6 px-4"
+                )}
+                key={index}
+              >
+                {content.content.map((listItem: any, itemIndex: number) =>
+                  listItem.content.map(
+                    (listContent: any, contentIndex: number) =>
+                      listContent.nodeType === "paragraph" &&
+                      listContent.content.map(
+                        (text: any, textIndex: number) => (
+                          <li
+                            className={clsx(
+                              "my-4",
+                              props.fields.isOrderedList
+                                ? "flex items-start"
+                                : "pl-5",
+                              "relative"
+                            )}
+                            key={textIndex}
+                          >
+                            {props.fields.isOrderedList && (
+                              <span className="absolute left-0">
+                                {itemIndex + 1}.
+                              </span>
+                            )}
+                            <span
+                              className={
+                                props.fields.isOrderedList ? "ml-8" : ""
+                              }
+                            >
+                              {text.value}
+                            </span>
+                          </li>
                         )
-                    )
-                  )}
-                </ul>
-              )
-          )}
-        </div>
+                      )
+                  )
+                )}
+              </ul>
+            )
+        )}
       </div>
-    );
-  };
-  
-  
-  
-  
+    </div>
+  );
+};
