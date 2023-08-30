9038f0a9d2bd964d7b70723198edb95739232bf4
Progress with the component <deMiguel@newsaktuell.de>
2023-08-30 11:40:34 +0200

    improved tables

--- app/components/pageComponents/medicos.tsx
+++ app/components/pageComponents/medicos.tsx
@@ -3,17 +3,16 @@ interface MedicosTableProps {
 }
 
 const MedicosTable: React.FC<MedicosTableProps> = ({ data }) => {
- 
   return (
     <table className="w-full table-fixed border-collapse text-sm md:text-base table mt-16 pb-24">
       <thead>
         <tr className="bg-gray-200">
-          <th className="py-2 text-center w-1/12">Especialidad</th>
-          <th className="py-2 text-center w-3/12">Nombre</th>
-          <th className="py-2 text-center w-2/12">Dirección</th>
-          <th className="py-2 text-center w-2/12">Teléfono</th>
-          <th className="py-2 text-center w-2/12">Mail o website</th>
-          <th className="py-2 text-center w-2/12">Idioma</th>
+          <th className="py-2 text-center w-1/12 sm:w-1/12">Especialidad</th>
+          <th className="py-2 text-center w-3/12 sm:w-3/12">Nombre</th>
+          <th className="py-2 text-center w-2/12 sm:w-2/12">Dirección</th>
+          <th className="py-2 text-center w-2/12 sm:w-2/12">Teléfono</th>
+          <th className="py-2 text-center w-3/12 sm:w-3/12">Mail o website</th>
+          <th className="py-2 text-center w-1/12 sm:w-1/12">Idioma</th>
         </tr>
       </thead>
       <tbody>
@@ -25,7 +24,9 @@ const MedicosTable: React.FC<MedicosTableProps> = ({ data }) => {
             >
               <td className="py-2 px-4">{medico.fields.especialidad}</td>
               <td className="py-2 px-4">{medico.fields.nombre}</td>
-              <td className="py-2 px-4">{medico.fields.direccion}</td>
+              <td className="py-2 px-4 hidden sm:table-cell">
+                {medico.fields.direccion}
+              </td>
               <td className="py-2 px-4">{medico.fields.telephone}</td>
               <td className="py-2 px-4">
                 <a
@@ -37,7 +38,9 @@ const MedicosTable: React.FC<MedicosTableProps> = ({ data }) => {
                   {medico.fields.emailwebsite}
                 </a>
               </td>
-              <td className="py-2 px-4">{medico.fields.idiomas}</td>
+              <td className="py-2 px-4 hidden sm:table-cell">
+                {medico.fields.idiomas}
+              </td>
             </tr>
           ) : null
         )}
