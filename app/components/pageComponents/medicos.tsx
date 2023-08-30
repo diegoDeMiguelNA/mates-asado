151dfd4295ea36f1a5e0ec53299e3562eff41524
Progress with the component <deMiguel@newsaktuell.de>
2023-08-30 13:03:55 +0200

    improved table

--- app/components/pageComponents/medicos.tsx
+++ app/components/pageComponents/medicos.tsx
@@ -4,48 +4,52 @@ interface MedicosTableProps {
 
 const MedicosTable: React.FC<MedicosTableProps> = ({ data }) => {
   return (
-    <table className="w-full table-fixed border-collapse text-sm md:text-base table mt-16 pb-24">
-      <thead>
-        <tr className="bg-gray-200">
-          <th className="py-2 text-center w-1/12 sm:w-1/12">Especialidad</th>
-          <th className="py-2 text-center w-3/12 sm:w-3/12">Nombre</th>
-          <th className="py-2 text-center w-2/12 sm:w-2/12">Dirección</th>
-          <th className="py-2 text-center w-2/12 sm:w-2/12">Teléfono</th>
-          <th className="py-2 text-center w-3/12 sm:w-3/12">Mail o website</th>
-          <th className="py-2 text-center w-1/12 sm:w-1/12">Idioma</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((medico: any, index: number) =>
-          medico.sys.contentType.sys.id === "medicoprofesionalDeLaSalud" ? (
-            <tr
-              key={medico.sys.id}
-              className={index % 2 === 0 ? "bg-gray-100" : ""}
-            >
-              <td className="py-2 px-4">{medico.fields.especialidad}</td>
-              <td className="py-2 px-4">{medico.fields.nombre}</td>
-              <td className="py-2 px-4 hidden sm:table-cell">
-                {medico.fields.direccion}
-              </td>
-              <td className="py-2 px-4">{medico.fields.telephone}</td>
-              <td className="py-2 px-4">
-                <a
-                  className="underline break-all"
-                  href={medico.fields.emailwebsite}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {medico.fields.emailwebsite}
-                </a>
-              </td>
-              <td className="py-2 px-4 hidden sm:table-cell">
-                {medico.fields.idiomas}
-              </td>
-            </tr>
-          ) : null
-        )}
-      </tbody>
-    </table>
+    <div className="w-full pb-24 flex flex-col items-center">
+      <table className="medicos-table w-full table-auto sm:table-fixed border-collapse text-sm md:text-base mt-16 max-w-[1200px]">
+        <thead>
+          <tr className="bg-gray-200">
+            <th className="py-2 text-center sm:w-2/12">Especialidad</th>
+            <th className="py-2 text-center sm:w-2/12">Nombre</th>
+            <th className="py-2 text-center sm:w-2/12">Dirección</th>
+            <th className="py-2 text-center sm:w-2/12">Teléfono</th>
+            <th className="py-2 text-center sm:w-2/12">
+              Mail o website
+            </th>
+            <th className="py-2 text-center w-1/12 sm:w-2/12">Idioma</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((medico: any, index: number) =>
+            medico.sys.contentType.sys.id === "medicoprofesionalDeLaSalud" ? (
+              <tr
+                key={medico.sys.id}
+                className={index % 2 === 0 ? "bg-gray-100" : ""}
+              >
+                <td className="py-2 sm:px-4 break-all">{medico.fields.especialidad}</td>
+                <td className="py-2 sm:px-4 break-all">{medico.fields.nombre}</td>
+                <td className="py-2 sm:px-4 break-all hidden sm:table-cell">
+                  {medico.fields.direccion}
+                </td>
+                <td className="py-2 sm:px-4 break-all">{medico.fields.telephone}</td>
+                <td className="py-2 sm:px-4 break-all">
+                  <a
+                    className="underline break-all"
+                    href={medico.fields.emailwebsite}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {medico.fields.emailwebsite}
+                  </a>
+                </td>
+                <td className="py-2 sm:px-4 break-all hidden sm:table-cell">
+                  {medico.fields.idiomas}
+                </td>
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- app/pages/medicos/page.tsx
+++ app/pages/medicos/page.tsx
@@ -13,22 +13,20 @@ export default async function Medicos() {
 
   return (
     <>
-      <main className="text-center mb-2sm:px-10 mb-8 sm:mb-24 mx-2 sm:mx-24 landscape:mb-2 landscape:px-2 landscape:mx-2">
-        <div className="mx-auto px-4 sm:px-2 lg:px-8">
-          <div className="pt-12 py-12">
-            <h2 className="text-2xl sm:text-5xl font-heading uppercase">
-              {title}
-            </h2>
-            <h3 className="text-sm mx-8 font-heading uppercase pt-10 mb-4 sm:mx-24">
-              {subtitle}
-            </h3>
-          </div>
-          <h3 className="warningText text-red-500 text-center mt-10 mb-24">
-            Por favor, gire su dispositivo a modo paisaje (landscape) para poder
-            ver la tabla con el contenido.
+      <main className="text-center flex flex-col items-center justify-center">
+        <div className="py-12">
+          <h2 className="text-2xl sm:text-5xl font-heading uppercase">
+            {title}
+          </h2>
+          <h3 className="text-sm mx-8 font-heading uppercase pt-10 mb-4 sm:mx-24">
+            {subtitle}
           </h3>
-          <MedicosTable data={pageBody} />
         </div>
+        <h3 className="warningText text-red-500 text-center mt-10 mb-24">
+          Por favor, gire su dispositivo a modo paisaje (landscape) para poder
+          ver la tabla con el contenido.
+        </h3>
+        <MedicosTable data={pageBody} />
       </main>
     </>
   );
