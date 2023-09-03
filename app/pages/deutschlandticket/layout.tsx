5342da39be2cc3da6528b0d799bb220966e7c0c3
Progress with the component <deMiguel@newsaktuell.de>
2023-09-03 13:38:14 +0200

    test open graph image

--- app/pages/deutschlandticket/layout.tsx
+++ app/pages/deutschlandticket/layout.tsx
@@ -1,4 +1,3 @@
-import { getAsset } from "@/lib/contentful/fetchDataFromContentful";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,16 +8,10 @@ export const metadata: Metadata = {
     url: "https://www.mates-asado.de/pages/deutschlandticket",
     siteName: "Mates&Asado",
     images: [
-      {
-        url: `https:${"//images.ctfassets.net/wdwnckov7fnq/1omR1b1SJKC6hm3eNYpMmH/9c0d9a4fd9b499e01408c364a68f9f97/deutschland_ticket_3.jpeg"}`,
-        width: 800,
-        height: 600,
-      },
       // {
-      //   url: "https://nextjs.org/og-alt.png",
-      //   width: 1800,
-      //   height: 1600,
-      //   alt: "My custom alt",
+      //   url: "https://images.ctfassets.net/wdwnckov7fnq/1omR1b1SJKC6hm3eNYpMmH/9c0d9a4fd9b499e01408c364a68f9f97/deutschland_ticket_3.jpeg",
+      //   width: 800,
+      //   height: 600,
       // },
     ],
     locale: "en_US",
---
+++ app/pages/deutschlandticket/opengraph-image.tsx
@@ -0,0 +1,8 @@
+export const alt = "Un tren regional alemán";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/jpeg";
+
+export default function Image() {}
