30c7ace1eb57231de691b48111a8a4e96b2aa213
Progress with the component <deMiguel@newsaktuell.de>
2023-09-03 14:01:12 +0200

    last try

--- app/pages/deutschlandticket/layout.tsx
+++ app/pages/deutschlandticket/layout.tsx
@@ -7,14 +7,6 @@ export const metadata: Metadata = {
       "Info sobre el Deutschland Ticket, como pedirlo, precios, etc.",
     url: "https://www.mates-asado.de/pages/deutschlandticket",
     siteName: "Mates&Asado",
-    images: [
-      {
-        // url: "https://images.ctfassets.net/wdwnckov7fnq/1omR1b1SJKC6hm3eNYpMmH/9c0d9a4fd9b499e01408c364a68f9f97/deutschland_ticket_3.jpeg",
-        width: 800,
-        height: 600,
-        url: "./opengraph-image.jpeg",
-      },
-    ],
     locale: "en_US",
     type: "website",
   },
