e3c6d3ad8fb62d416790c9d81f5cf00adc6bda05
Progress with the component <deMiguel@newsaktuell.de>
2023-09-05 12:47:17 +0200

    added route revalidation to fetch data every 10 seconds

---
+++ app/route.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const revalidate = 10
+export const fetchCache = 'auto'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
+export const maxDuration = 5
+ 
+export default function MyComponent() {}
