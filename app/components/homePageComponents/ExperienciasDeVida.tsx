68c8c8560eecfd6b57fb9c35172a55c59ef1b70e
Progress with the component <deMiguel@newsaktuell.de>
2023-09-06 16:24:40 +0200

    changed colot

--- app/components/homePageComponents/ExperienciasDeVida.tsx
+++ app/components/homePageComponents/ExperienciasDeVida.tsx
@@ -77,7 +77,7 @@ export default async function ExperienciasDeVida() {
             </p>
             <Link href="/experiencias">
               <Button
-                className="bg-regular-teal text-xxs md:text-xs lg:text-[12px] rounded-full mt-4 py-4"
+                className="bg-regular-red text-xxs md:text-xs lg:text-[12px] rounded-full mt-4 py-4"
                 size="customPill"
                 style={{ color: "white" }}
               >
