4b0b2eb97f677c870887ca66d8620cac5394d2d6
Joo Han Kim <joo-han.kim@hermesworld.com>
2023-09-07 09:15:33 +0200

    Fixed responsible behavior and some refactoring

--- app/components/pageComponents/medicos.tsx
+++ app/components/pageComponents/medicos.tsx
@@ -9,25 +9,28 @@ import {
   CardHeader,
   CardTitle,
 } from "../card/card";
-import { HomeIcon, PhoneIcon, Globe2Icon, LanguagesIcon } from "lucide-react";
+import { icons } from "lucide-react";
 import { ReactNode } from "react";
 
 interface MedicosListProps {
   data: IMedicoprofesionalDeLaSalud[];
 }
 
-function IconWrapper({ children }: { children: ReactNode }) {
+function Icon({ name, size = 24 }: { name: string, size: string | number }) {
+  const LucideIcon = icons[name as keyof typeof icons]
+  if (!LucideIcon) return null
+
   return (
-    <div className="text-gray-400 inline mr-1 w-6 flex-shrink-0 flex-grow-0">
-      {children}
+    <div className="text-gray-400 mr-1 w-6 flex-shrink-0 flex-grow-0">
+      <LucideIcon size={size} className="pt-0.5" />
     </div>
   );
 }
 
-function Detail({ icon, content }: { icon: ReactNode; content: ReactNode }) {
+function Detail({ iconName, content }: { iconName: string; content: ReactNode }) {
   return (
     <div className="flex text-ellipsis overflow-hidden w-full">
-      <IconWrapper>{icon}</IconWrapper>
+      <Icon name={iconName} size={24} />
       <div>{content}</div>
     </div>
   );
@@ -43,10 +46,10 @@ function MedicosCard({ data }: { data: IMedicoprofesionalDeLaSaludFields }) {
         <CardDescription>{especialidad}</CardDescription>
       </CardHeader>
       <CardContent className="text-left">
-        <Detail icon={<HomeIcon />} content={direccion} />
-        <Detail icon={<PhoneIcon />} content={telephone} />
+        <Detail iconName="Home" content={direccion} />
+        <Detail iconName="Phone" content={telephone} />
         <Detail
-          icon={<Globe2Icon />}
+          iconName="Globe2"
           content={
             <div className="w-full">
               <a
@@ -60,7 +63,7 @@ function MedicosCard({ data }: { data: IMedicoprofesionalDeLaSaludFields }) {
             </div>
           }
         />
-        <Detail icon={<LanguagesIcon />} content={idiomas} />
+        <Detail iconName="Languages" content={idiomas} />
       </CardContent>
     </Card>
   );
@@ -71,7 +74,7 @@ const MedicosList: React.FC<MedicosListProps> = ({ data }) => {
     (el) => el.sys.contentType.sys.id === "medicoprofesionalDeLaSalud"
   );
   return (
-    <div className="w-full pb-24 grid grid-cols-3 gap-4 px-72">
+    <div className="w-full pb-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-2 sm:px-16">
       {filtered.map(({ sys, fields }) => (
         <MedicosCard data={fields} key={sys.id} />
       ))}
--- app/medicos/page.tsx
+++ app/medicos/page.tsx
@@ -1,27 +1,22 @@
-import { IMedicoprofesionalDeLaSalud } from "@/@types/generated/contentful";
+import {
+  IMedicoprofesionalDeLaSalud
+} from "@/@types/generated/contentful";
 import MedicosList from "@/app/components/pageComponents/medicos";
 import { getReusablePage } from "@/lib/contentful/fetchDataFromContentful";
-import { Entry } from "contentful";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
+type MedicosPageContent = {
+  title: string;
+  subtitle: string;
+  pageBody: IMedicoprofesionalDeLaSalud[];
+}
+
 export default async function Medicos() {
-<<<<<<< HEAD:app/pages/medicos/page.tsx
   const {
     fields: { title, subtitle, pageBody },
-  }: Entry<{
-    title: string;
-    subittle: string;
-    pageBody: IMedicoprofesionalDeLaSalud[];
-  }> = await getReusablePage("7815QrUkSC1g3On5DHk7Me");
-=======
-  const results: Entry<IFuehrerscheinReusableFields> = await getReusablePage(
-    "7815QrUkSC1g3On5DHk7Me"
-  );
-
-  const { title, subtitle, pageBody } = results.fields;
->>>>>>> main:app/medicos/page.tsx
+  } = await getReusablePage<MedicosPageContent>("7815QrUkSC1g3On5DHk7Me");
 
   return (
     <>
@@ -34,10 +29,6 @@ export default async function Medicos() {
             {subtitle}
           </h3>
         </div>
-        {/* <h3 className="warningText text-red-500 text-center mt-10 mb-24">
-          Por favor, gire su dispositivo a modo paisaje (landscape) para poder
-          ver la tabla con el contenido.
-        </h3> */}
         <MedicosList data={pageBody} />
       </main>
     </>
