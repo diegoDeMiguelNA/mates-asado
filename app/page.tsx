b76105012262af3710b64873945672e347bfda21
Progress with the component <deMiguel@newsaktuell.de>
2023-09-14 15:01:35 +0200

    error handling for the Novedades modal. If publication is unpublished on contentful the app doesn't break

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import { getAsset, getHomeIcons } from "@/lib/contentful/fetchDataFromContentful";
+import {
+  getAsset,
+  getHomeIcons,
+} from "@/lib/contentful/fetchDataFromContentful";
 import HeaderHome from "./components/header/headerHome";
 import AboutUs from "./components/homePageComponents/AboutUs";
 import ExperienciasDeVida from "./components/homePageComponents/ExperienciasDeVida";
@@ -10,15 +13,21 @@ export default async function Home() {
     fields: { homeIconComponent },
   } = await getHomeIcons();
 
-  const { fields } = await getAsset("1SsCd0t1OAuqBzCgDVK2VJ");
+  let fields;
 
+  try {
+    const assetData = await getAsset("1SsCd0t1OAuqBzCgDVK2VJ");
+    fields = assetData.fields;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
   return (
     <>
       <HeaderHome />
       <div className="homePage text-center flex flex-col items-center">
         <HomeProps navigationElements={homeIconComponent} />
 
-        <Novedades fields={fields} />
+        {fields && <Novedades fields={fields} />}
 
         <ExperienciasDeVida />
 
