400087c1751c65ba991d2a68898decbd90982b97
Progress with the component <deMiguel@newsaktuell.de>
2023-09-21 11:53:17 +0200

    solved issue on contenful client

--- getContentfulEnvironment.ts
+++ getContentfulEnvironment.ts
@@ -2,7 +2,8 @@ require("dotenv").config();
 const contentfulManagement = require("contentful-management");
 require("dotenv").config({ path: __dirname + "/.env.local" });
 
-const { CONTENTFUL_SPACE, CONTENTFUL_ACCESS_TOKEN_TYPESCRIPT } = process.env;
+const { NEXT_PUBLIC_CONTENTFUL_SPACE, CONTENTFUL_ACCESS_TOKEN_TYPESCRIPT } =
+  process.env;
 
 module.exports = function () {
   const contentfulClient = contentfulManagement.createClient({
@@ -10,7 +11,7 @@ module.exports = function () {
   });
 
   return contentfulClient
-    .getSpace(CONTENTFUL_SPACE)
+    .getSpace(NEXT_PUBLIC_CONTENTFUL_SPACE)
     .then((space: { getEnvironment: (arg0: string | undefined) => any }) =>
       space.getEnvironment("master")
     );
