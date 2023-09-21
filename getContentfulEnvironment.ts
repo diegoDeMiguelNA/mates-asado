b316b51ee4c1ccabe20b99262f85a6c5562be0f6
Progress with the component <deMiguel@newsaktuell.de>
2023-09-21 11:46:06 +0200

    some progress

--- getContentfulEnvironment.ts
+++ getContentfulEnvironment.ts
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 require("dotenv").config();
 const contentfulManagement = require("contentful-management");
 require("dotenv").config({ path: `${__dirname}/.env.local` });
 
 const { CONTENTFUL_SPACE, CONTENTFUL_ACCESS_TOKEN_TYPESCRIPT } = process.env;
 
+// eslint-disable-next-line func-names
 module.exports = function () {
   const contentfulClient = contentfulManagement.createClient({
     accessToken: CONTENTFUL_ACCESS_TOKEN_TYPESCRIPT,
@@ -11,7 +13,7 @@ module.exports = function () {
 
   return contentfulClient
     .getSpace(CONTENTFUL_SPACE)
-    .then((space: { getEnvironment: (arg0: string | undefined) => any }) =>
+    .then((space: { getEnvironment: (arg0: string | undefined) => never }) =>
       space.getEnvironment("master"),
     );
 };
