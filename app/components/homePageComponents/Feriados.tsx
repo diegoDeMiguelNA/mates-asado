5722b039f2e1c084a72aab9b8959a76fa0b9d3a5
Progress with the component <deMiguel@newsaktuell.de>
2023-10-31 06:19:03 +0100

    added elecciones on main page

--- app/components/homePageComponents/Feriados.tsx
+++ app/components/homePageComponents/Feriados.tsx
@@ -8,15 +8,15 @@ import { getTextFromContentfulRichText } from "./ExperienciasDeVida";
 
 export default async function Elecciones() {
   const results: Entry<IBlogSubtitleParagraphAndImageFields> =
-    await getSubtitleAndParagraphAndImage("2JpIBOhInLqgcOOxS7yaeb");
+    await getSubtitleAndParagraphAndImage("4FDkPymFSxK5sHjXiMdPYf");
 
   const { subtitle, paragraph, assets } = results.fields;
 
   if (!results) return null;
 
   return (
-    <div className="elecciones flex flex-col px-4 md:px-8 justify-center items-center mb-12 md:mb-24 pb-12 md:pb-24 lg:mb-[20px] lg:pt-[90px] bg-light-yellow">
-      <div className="relative h-[160px] w-[320px] sm:w-[480px] sm:h-[350px] md:w-[600px] md:h-[350px] lg:w-[850px] overflow-hidden">
+    <div className="elecciones flex flex-col px-4 md:px-8 justify-center items-center mb-12 md:mb-12 pb-12 md:pb-24 lg:mb-[100px] lg:pt-[90px] bg-light-yellow">
+      <div className="relative h-[160px] w-[320px] sm:w-[480px] sm:h-[350px] md:w-[600px] md:h-[350px] lg:w-[850px] overflow-hiddenx">
         <Image
           src={
             assets
@@ -31,25 +31,21 @@ export default async function Elecciones() {
           fill
           style={{ objectFit: "cover", zIndex: 0 }}
         />
-        <div className="bg-black opacity-10 absolute top-0 left-0 w-full h-full" />
+        <div className="bg-black opacity-40 absolute top-0 left-0 w-full h-full" />
         <div className="p-4 w-full h-full absolute flex flex-col justify-center items-center">
           <h3
-            className="text-lg md:text-s lg:text-xl text-yellow font-bold md:mb-4 text-center"
+            className="text-lg md:text-s lg:text-2xl text-white font-bold md:mb-4 text-center "
             style={{ color: "#FFFFFF" }}
           >
             {subtitle}
           </h3>
           <p
-            className="text-xs text-white font-bold md:text-sm md:px-2 mb-2 md:mb-4 max-w-[800px] text-center"
+            className="text-xs text-white font-bold md:text-lg md:px-2 mb-2 md:mb-4 max-w-[800px] text-center"
             style={{ lineHeight: "1.5rem" }}
           >
             {paragraph && getTextFromContentfulRichText(paragraph)}
           </p>
-          <Link
-            className="mt-2 md:mt-4"
-            href="/elecciones-presidenciales-2023"
-            passHref
-          >
+          <Link className="mt-2 md:mt-4" href="/feriados" passHref>
             <Button
               className="bg-regular-teal text-xxxs md:text-xxs lg:text-[12px] rounded-full py-2 md:py-4"
               size="customPill"
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import HeaderHome from "./components/header/headerHome";
 import AboutUs from "./components/homePageComponents/AboutUs";
 import Elecciones from "./components/homePageComponents/EleccionesPresidencaiales";
 import ExperienciasDeVida from "./components/homePageComponents/ExperienciasDeVida";
+import Feriados from "./components/homePageComponents/Feriados";
 import HomeProps from "./components/homePageComponents/home-props";
 
 export default async function Home() {
@@ -15,6 +16,8 @@ export default async function Home() {
         <HomeProps navigationElements={navigationElements} />
 
         <Elecciones />
+
+        <Feriados />
 
         <ExperienciasDeVida />
 
